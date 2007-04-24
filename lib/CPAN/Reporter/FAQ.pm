86233a82faccf9c2eceab58ac067393059e29b46
David Golden <dagolden@cpan.org>
2007-04-24 04:32:08 +0000

    preparing for 0.42
    git-svn-id: https://dagolden.googlecode.com/svn/CPAN-Reporter/trunk@962 dfce27d5-b31c-0410-bb09-030b4413eeba

--- lib/CPAN/Reporter/FAQ.pm
+++ lib/CPAN/Reporter/FAQ.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter::FAQ
 # Not really a .pm file, but holds wikidoc which will be
 # turned into .pod by the Build.PL
-$VERSION = "0.41";
+$VERSION = "0.42";
 use strict; # make CPANTS happy
 1;
 __END__
