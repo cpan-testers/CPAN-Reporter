a6e84a67c93a8da6ed65c0c56b0f1a709c9389a3
David Golden <dagolden@cpan.org>
2008-06-05 06:37:58 -0400

    dont set PERL5OPT with a leading space

--- lib/CPAN/Reporter.pm
+++ lib/CPAN/Reporter.pm
@@ -141,7 +141,8 @@ HERE
     {
       # ensure autoflush
       local $ENV{PERL5OPT} = $ENV{PERL5OPT} || q{};
-      $ENV{PERL5OPT} .= ' -MDevel::Autoflush ';
+      $ENV{PERL5OPT} .= q{ } if length $ENV{PERL5OPT};
+      $ENV{PERL5OPT} .= '-MDevel::Autoflush';
       tee($tee_input, { stderr => 1 }, $temp_out);
     }
 
