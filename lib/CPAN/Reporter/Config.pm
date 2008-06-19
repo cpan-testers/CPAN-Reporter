66dd18aeaada7cd7f388f73c05c2a735e0c56861
David Golden <dagolden@cpan.org>
2008-06-19 19:27:39 -0400

    bumped version to 1.15_56

--- lib/CPAN/Reporter/Config.pm
+++ lib/CPAN/Reporter/Config.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter::Config;
 use strict; 
 use vars qw/$VERSION/;
-$VERSION = '1.15_55'; 
+$VERSION = '1.15_56'; 
 $VERSION = eval $VERSION;
 
 use Config::Tiny ();
--- lib/CPAN/Reporter/PrereqCheck.pm
+++ lib/CPAN/Reporter/PrereqCheck.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter::PrereqCheck;
 use strict;
 use vars qw/$VERSION/;
-$VERSION = '1.15_55'; 
+$VERSION = '1.15_56'; 
 $VERSION = eval $VERSION;
 
 use ExtUtils::MakeMaker;
