1800bb5d2f693041700b18cbfc6fabafe81bfab8
David Golden <dagolden@cpan.org>
2008-12-07 06:26:50 -0500

    bump prereqs; bump version; prep for release

--- lib/CPAN/Reporter.pm
+++ lib/CPAN/Reporter.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter;
 use strict;
 use vars qw/$VERSION/;
-$VERSION = '1.1703'; 
+$VERSION = '1.1704'; 
 $VERSION = eval $VERSION; ## no critic
 
 use Config;
--- lib/CPAN/Reporter/History.pm
+++ lib/CPAN/Reporter/History.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter::History;
 use strict; 
 use vars qw/$VERSION @ISA @EXPORT_OK/;
-$VERSION = '1.1703'; 
+$VERSION = '1.1704'; 
 $VERSION = eval $VERSION; ## no critic
 
 use Config;
