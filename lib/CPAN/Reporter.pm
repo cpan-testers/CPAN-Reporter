0d55d60d8a65d21713cc30e2bf4de3e39b111980
David Golden <dagolden@cpan.org>
2009-11-10 08:54:06 -0500

    bump version to 1.1709

--- lib/CPAN/Reporter.pm
+++ lib/CPAN/Reporter.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter;
 use strict;
 use vars qw/$VERSION/;
-$VERSION = '1.1708'; 
+$VERSION = '1.1709'; 
 $VERSION = eval $VERSION; ## no critic
 
 use Config;
--- lib/CPAN/Reporter/History.pm
+++ lib/CPAN/Reporter/History.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter::History;
 use strict; 
 use vars qw/$VERSION @ISA @EXPORT_OK/;
-$VERSION = '1.1708'; 
+$VERSION = '1.1709'; 
 $VERSION = eval $VERSION; ## no critic
 
 use Config;
