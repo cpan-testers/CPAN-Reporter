40cfc1920e1254e79be1ed6a4bc2098e2f427401
David Golden <dagolden@cpan.org>
2010-02-17 13:52:06 -0500

    bump version to 1.1711

--- lib/CPAN/Reporter/History.pm
+++ lib/CPAN/Reporter/History.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter::History;
 use strict; 
 use vars qw/$VERSION @ISA @EXPORT_OK/;
-$VERSION = '1.1710';
+$VERSION = '1.1711';
 $VERSION = eval $VERSION; ## no critic
 
 use Config;
