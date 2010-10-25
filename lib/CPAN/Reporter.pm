3300d6b1940193b3ce9fb2d1c5c7202738f7f560
David Golden <dagolden@cpan.org>
2010-10-24 20:57:56 -0400

    bump version to 1.18_04

--- lib/CPAN/Reporter.pm
+++ lib/CPAN/Reporter.pm
@@ -1,7 +1,7 @@
 package CPAN::Reporter;
 use strict;
 use vars qw/$VERSION/;
-$VERSION = '1.1803';
+$VERSION = '1.18_04';
 $VERSION = eval $VERSION; ## no critic
 
 use Config;
--- lib/CPAN/Reporter/API.pm
+++ lib/CPAN/Reporter/API.pm
@@ -3,7 +3,7 @@ package CPAN::Reporter::API;
 # turned into .pod by the Build.PL
 use strict; # make CPANTS happy
 use vars qw/$VERSION/;
-$VERSION = '1.1803';
+$VERSION = '1.18_04';
 $VERSION = eval $VERSION; ## no critic
 1;
 __END__
